91f028672a0ab1d3d1bc04ee012f7d4e72c42636
Tkres <a.tkres@gmail.com>
2012-09-17 17:12:19 +1000

    Added Client, Server

---
+++ src/Client.java
@@ -0,0 +1,63 @@
+import netP5.NetAddress;
+import oscP5.OscMessage;
+import oscP5.OscP5;
+import processing.core.PApplet;
+
+public class Client extends PApplet {
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String args[]) {
+		PApplet.main(new String[] { "--present", "PSketch" });
+	}
+
+	// Core Variables
+	int swidth, sheight;
+	int rootFrame = 0;
+
+	// Communication
+	OscP5 oscP5;
+	NetAddress myTargetLocation;
+	int target_port = 12000;
+	String target_ip0 = "127.0.0.1";
+	String target_ip1 = "192.168.0.194";
+	String target_ip2 = "172.19.19.109";
+	String target_ip = target_ip0;
+	int home_port = 13000 + (int) random(1000);
+	String home_ip;
+	
+	public void setup() {
+		swidth = 640;
+		sheight = 480;
+		size(swidth, sheight);
+		colorMode(HSB, 100);
+		smooth();
+
+		// initialize communication
+		oscP5 = new OscP5(this, home_port);
+		home_ip = oscP5.ip();
+		myTargetLocation = new NetAddress(target_ip, target_port);
+		this.sendMyLocationToTarget();
+		
+	}
+
+
+	public void draw() {
+		
+
+	}
+	
+	// -----------------------------------------------------------------------------
+	// COMMUNICATION.
+	public void sendMyLocationToTarget() {
+		OscMessage myMessage = new OscMessage("/myLocation");
+		myMessage.add(home_ip);
+		myMessage.add(home_port);
+		oscP5.send(myMessage, myTargetLocation);
+	}
+	
+	public void oscEvent(OscMessage theOscMessage) {
+		String addrPattern = theOscMessage.addrPattern();
+
+	}
+	
+}
---
+++ src/Server.java
@@ -0,0 +1,73 @@
+import java.util.ArrayList;
+
+import netP5.NetAddress;
+import oscP5.OscMessage;
+import oscP5.OscP5;
+import processing.core.PApplet;
+import processing.core.PFont;
+import processing.core.PVector;
+
+public class Server extends PApplet {
+	private static final long serialVersionUID = 1L;
+	public static void main(String args[]) {
+		PApplet.main(new String[] { "--present", "PSketch" });
+	}
+	
+	int swidth, sheight;
+	
+	OscP5 oscP5;
+	int home_port = 12000;
+	String home_ip;
+	int target_port = 13001;
+	String target_ip = "127.0.0.1";
+	ArrayList<NetAddress> targetLocations;
+	
+
+	// -----------------------------------------------------------------------------
+	// SETUP.
+	
+	public void setup() {
+		swidth = 800; 
+		sheight = 600;
+		size(swidth, sheight);
+		colorMode(HSB,100);
+		smooth();
+		
+		targetLocations = new ArrayList<NetAddress>();
+		
+		oscP5 = new OscP5(this,home_port);
+		home_ip = oscP5.ip();
+		println("Server IP: "+home_ip);
+		println("Server Port: "+home_port);
+		
+	}
+	
+	
+	// -----------------------------------------------------------------------------
+	
+	public void draw() {
+		
+	}
+	
+	
+	// -----------------------------------------------------------------------------
+	// COMMUNICATIONS.
+	// COMMUNICATIONS: HANDLE INCOMING MESSAGES
+	public void oscEvent(OscMessage theOscMessage) {
+	  String addrPattern = theOscMessage.addrPattern();
+	  if (addrPattern.equals("/myLocation")) {
+		  this.parseMessageMyLocation(theOscMessage);
+	  }
+	}
+	
+	// COMMUNICATIONS: PARSE MESSAGES
+	public void parseMessageMyLocation(OscMessage theOscMessage) {
+		// Add Client to list of targetLocations.
+		String client_ip = theOscMessage.get(0).stringValue();
+		int client_port = theOscMessage.get(1).intValue();
+		NetAddress newTargetLocation = new NetAddress(client_ip, client_port);
+		targetLocations.add(newTargetLocation);
+		System.out.println("Added Client." + newTargetLocation);
+	}
+	
+}
